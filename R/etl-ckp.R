3aaaffe237d79a813c618ee8b07f4bd9f44b608a
Laurie Platt <47427223+laurie-platt@users.noreply.github.com>
2023-05-24 07:20:17 +0100

    Demo ETL part of pipeline

---
+++ R/etl-ckp.R
@@ -0,0 +1,12 @@
+library(here)
+library(tidyverse)
+library(janitor)
+
+ethnic_csv <- here("pipeline", "raw", "sheffield-custom-area-ethnic-group.csv")
+
+ethnic_group <- read_csv(ethnic_csv, skip = 6) |>
+  clean_names()
+
+ckp_data_model <- here("pipeline", "processed", "ckp.RData")
+
+save(ethnic_group, file = ckp_data_model)
