465b9dc6c2d9bb04a38efd89639ab0123b27ace3
Laurie Platt <47427223+laurie-platt@users.noreply.github.com>
2023-05-25 21:55:11 +0100

    Example of 1 v. basic community profile

--- R/etl-ckp.R
+++ R/etl-ckp.R
@@ -5,7 +5,8 @@ library(janitor)
 ethnic_csv <- here("pipeline", "raw", "sheffield-custom-area-ethnic-group.csv")
 
 ethnic_group <- read_csv(ethnic_csv, skip = 6) |>
-  clean_names()
+  clean_names() |>
+  select(-variable)
 
 ckp_data_model <- here("pipeline", "processed", "ckp.RData")
 
