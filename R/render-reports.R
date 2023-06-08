389529702b022113cdecf7f67ea2bd2cfdf8d928
Laurie Platt <47427223+laurie-platt@users.noreply.github.com>
2023-06-08 19:37:03 +0100

    Better code comments

--- R/render-reports.R
+++ R/render-reports.R
@@ -7,17 +7,17 @@ library(furrr)
 
 render_reports <- function(community_name){
 
-  # Create new name for .qmd copy
+  # Create new name for Quarto (.qmd) copy
   community_qmd <- here("R", str_c("report-layout-", community_name, ".qmd"))
 
-  # Create copies of the layout files using the modified file names
+  # Create copy of the layout file using the modified file name
   file.copy(
     from = here("R", "report-layout.qmd"),
     to = community_qmd,
     overwrite = TRUE
   )
 
-  # Render reports using .qmd copies.
+  # Render report (.pdf) using Quarto copy
   # TODO: Use Quarto "projects" to render to a different folder i.e. \output
   # https://quarto.org/docs/projects/quarto-projects.html
   quarto::quarto_render(
@@ -26,14 +26,14 @@ render_reports <- function(community_name){
     output_file = str_c("report-for-", community_name, ".pdf")
   )
 
-  # Remove .qmd copies
+  # Remove Quarto copy
   file.remove(community_qmd)
 }
 
-# Set options and cores
+# Set options and cores for parallel processing
 plan(cluster, workers = 3)
 
-# Future_walk the create_reports function
+# Iterate through our different communities
 # TODO: Swap sample Iris dataset and different "species",
 #       for our CKP dataset and different "communities"
 as.character(unique(iris$Species)) %>%
