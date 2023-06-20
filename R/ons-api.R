4a92fcdd4659a619cfa89b47c59540e0cb2f6426
Laurie Platt <47427223+laurie-platt@users.noreply.github.com>
2023-06-20 14:29:31 +0100

    Data from ONS via API

--- R/ons-api.R
+++ R/ons-api.R
@@ -1,20 +1,47 @@
 library(onsr)
 library(tidyverse)
 library(janitor)
+library(here)
 
 la_code <- "E08000019"
 la_name <- "Sheffield"
 
-ons_datasets <- ons_datasets()
-ons_ids <- ons_ids()|>
-  as_tibble()
+ons_datasets <- ons_datasets() |>
+  select(id, description)
 
-sexual_orientation_id <- "TS079"
-sexual_orientation_href <- ons_latest_href(id = sexual_orientation_id)
-sexual_orientation_version <- ons_latest_version(id = sexual_orientation_id)
-sexual_orientation_edition <- ons_latest_edition(id = sexual_orientation_id)
+get_data <- function(dataset_id, la_name = "Sheffield") {
+  df <- ons_get(id = dataset_id) |>
+    clean_names() |>
+    filter(lower_tier_local_authorities == la_name) |>
+    select(-starts_with("lower")) |>
+    rename_with(~ return("code"), ends_with("_code")) |>
+    rename_with(~ return("category"), ends_with("_categories")) |>
+    filter(category != "Does not apply")
+}
 
-sexual_orientation <- ons_get(id = sexual_orientation_id) |>
-  filter(`Lower tier local authorities` == la_name) |>
-  clean_names() |>
-  select(-starts_with("lower"))
+sexual_orientation <- get_data("TS079")
+gender_identity <- get_data("TS078")
+multi_religion <- get_data("TS075")
+veteran <- get_data("TS074")
+armed_forces <- get_data("TS073")
+
+community <- tibble::tribble(
+  ~community_name,      ~community_code,
+  "Sexual orientation", "sexual_orientation",
+  "Gender identity",    "gender_identity",
+  "Muti-religion",      "multi_religion",
+  "Veteran",            "veteran",
+  "Armed forces",       "armed_forces"
+)
+
+ons_data <- here("pipeline", "processed", "ons.RData")
+
+save(
+  community,
+  sexual_orientation,
+  gender_identity,
+  multi_religion,
+  veteran,
+  armed_forces,
+  file = ons_data
+)
